5613a24cfb3e887e85d7a2217dd69dc7bc7794c9
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-03-11 19:32:16 +0100

    Add project files.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj n");
+            int n = int.Parse(Console.ReadLine());
+            int wynik =0;
+            for (int i = 1; i <= n; i++)
+            {
+                wynik = (i % 2 == 1) ? wynik += i : wynik -= i;
+            }
+            Console.WriteLine(wynik);
+        }
+    }
+}
